498626a08803b391a4d4d5818f29aa7b7fc3c813
Sandro Stikić <sandro@stikic.me>
2020-02-02 15:52:06 +0800

    Add push block

--- Assets/Scripts/InteractivePhysics.cs
+++ Assets/Scripts/InteractivePhysics.cs
@@ -3,16 +3,13 @@ using System.Collections;
 using Prime31;
 
 
-public class PlayerPhysics : MonoBehaviour {
+public class InteractivePhysics : MonoBehaviour {
 	// movement config
 	public float gravity = -25f;
 	public float runSpeed = 8f;
-	public float pushSpeed = 4f;
 	public float groundDamping = 20f; // how fast do we change direction? higher means faster
 	public float inAirDamping = 5f;
 	public float jumpHeight = 3f;
-	public float pickupReset = 0.50f;
-
 
 	[HideInInspector]
 	private float normalizedHorizontalSpeed = 0;
@@ -20,10 +17,7 @@ public class PlayerPhysics : MonoBehaviour {
 	private CharacterController2D _controller;
 	private Animator _animator;
 	private RaycastHit2D _lastControllerColliderHit;
-	private Vector3 _velocity;
-	private GameObject block;
-	private float blockPickupTime;
-	private float blockGap = 2.25f;
+	public Vector3 _velocity;
 
 
 	void Awake() {
@@ -59,21 +53,7 @@ public class PlayerPhysics : MonoBehaviour {
 		//Debug.Log("onTriggerExitEvent: " + col.gameObject.name);
 	}
 
-    void onTriggerStayEvent(Collider2D col) {
-		if (col.tag == "Pushable") {
-			if (Input.GetKey(KeyCode.RightArrow)) {
-				col.GetComponent<InteractivePhysics>()._velocity +=
-	                new Vector3(pushSpeed, 0, 0);
-			} else if (Input.GetKey(KeyCode.LeftArrow)) {
-				col.GetComponent<InteractivePhysics>()._velocity -=
-	                new Vector3(pushSpeed, 0, 0);
-			}
-
-            if (Input.GetKey(KeyCode.Space) && Time.time > blockPickupTime + pickupReset) {
-				block = col.gameObject;
-				blockPickupTime = Time.time;
-            }
-		}
+	void onTriggerStayEvent(Collider2D col) {
 	}
 
 	#endregion
@@ -81,57 +61,11 @@ public class PlayerPhysics : MonoBehaviour {
 
 	// the Update loop contains a very simple example of moving the character around and controlling the animation
 	void Update() {
-        if (block != null) {
-			var pos = transform.position;
-			pos.y += blockGap;
-			block.transform.position = pos;
-			block.GetComponent<InteractivePhysics>()._velocity = Vector3.zero;
-		}
-
-        if (block != null && Time.time > blockPickupTime + pickupReset &&
-			Input.GetKey(KeyCode.Space)) {
-			var isLeft = this.transform.lossyScale.x > 0 ? true : false;
-			var xOffset = isLeft ? 2.0f : -2.0f;
-
-			block.transform.position = transform.position + new Vector3(xOffset, blockGap, 0.0f);
-			block = null;
-        }
-
-		if (_controller.isGrounded)
+		if (_controller.isGrounded) {
 			_velocity.y = 0;
-
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			normalizedHorizontalSpeed = 1;
-			if (transform.localScale.x < 0f)
-				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-
-			if (_controller.isGrounded) {
-				//_animator.Play(Animator.StringToHash("Run"));
-			}
-		} else if (Input.GetKey(KeyCode.LeftArrow)) {
-			normalizedHorizontalSpeed = -1;
-			if (transform.localScale.x > 0f)
-				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-
-			if (_controller.isGrounded) {
-				//_animator.Play(Animator.StringToHash("Run"));
-			}
-		} else {
-			normalizedHorizontalSpeed = 0;
-
-			if (_controller.isGrounded) {
-				//_animator.Play(Animator.StringToHash("Idle"));
-			}
 		}
 
-
-		// we can only jump whilst grounded
-		if (_controller.isGrounded && Input.GetKeyDown(KeyCode.UpArrow)) {
-			_velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
-			transform.SetParent(null);
-			//_animator.Play(Animator.StringToHash("Jump"));
-		}
-
+		//normalizedHorizontalSpeed = 0;
 
 		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
 		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
