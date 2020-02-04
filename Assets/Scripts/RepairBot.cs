70bec0470f986b8f2ebf22c2a8f2d4912c10818a
Peter <petercrabbe92@gmail.com>
2020-02-04 23:29:20 +0800

    Initial Commit

---
+++ Assets/Scripts/RepairBot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairBot : MonoBehaviour
+{
+    public uint repairBot = 1;
+    Animator m_animator;
+    SpriteRenderer m_spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spr = this.gameObject.GetComponent<SpriteRenderer>();
+        m_animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BotRepair()
+    {
+        ++repairBot;
+        //m_spr.sprite = Resources.Load()
+        m_animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(LoadAnimator());
+    }
+
+    string LoadAnimator()
+    {
+        if(repairBot == 1)
+        {
+            return "Animations/p1_player_0";
+        } else if (repairBot == 2)
+        {
+            return "Animations/p2_player";
+        } else if (repairBot == 3)
+        {
+            return "Animations/p3_player";
+        } else if (repairBot == 4)
+        {
+            //The person who wrote that in all upper case should die.
+            return "Animations/p4_PLAYER";
+        } else if (repairBot == 5)
+        {
+            //NO PLAYER ANIMATOR FOR P5
+            return "Animations/p4_PLAYER";
+        } else
+        {
+            return "Animations/p1_player_0";
+        }
+    }
+}
