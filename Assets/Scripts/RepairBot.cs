b4cc0548fd4b0583aea860dd6f5335d359c17fcc
Peter <petercrabbe92@gmail.com>
2020-02-07 10:48:09 +0800

    who knows

--- Assets/Scripts/RepairBot.cs
+++ Assets/Scripts/RepairBot.cs
@@ -7,48 +7,51 @@ public class RepairBot : MonoBehaviour
     public uint repairBot = 1;
     Animator m_animator;
     SpriteRenderer m_spr;
+    PlayerPhysics m_pp;
     // Start is called before the first frame update
     void Start()
     {
         m_spr = this.gameObject.GetComponent<SpriteRenderer>();
         m_animator = this.gameObject.GetComponent<Animator>();
+        m_pp = this.gameObject.GetComponent<PlayerPhysics>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void BotRepair()
     {
         ++repairBot;
-        //m_spr.sprite = Resources.Load()
-        m_animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(LoadAnimator());
-    }
-
-    string LoadAnimator()
-    {
-        if(repairBot == 1)
+        string loaddir;
+        if (repairBot == 1)
         {
-            return "Animations/p1_player_0";
-        } else if (repairBot == 2)
+            loaddir = "Animations/p1_player_0";
+        }
+        else if (repairBot == 2)
         {
-            return "Animations/p2_player";
-        } else if (repairBot == 3)
+            loaddir = "Animations/p2_player";
+        }
+        else if (repairBot == 3)
         {
-            return "Animations/p3_player";
-        } else if (repairBot == 4)
+            loaddir = "Animations/p3_player";
+        }
+        else if (repairBot == 4)
         {
             //The person who wrote that in all upper case should die.
-            return "Animations/p4_PLAYER";
-        } else if (repairBot == 5)
+            loaddir = "Animations/p4_PLAYER";
+        }
+        else if (repairBot == 5)
         {
             //NO PLAYER ANIMATOR FOR P5
-            return "Animations/p4_PLAYER";
-        } else
-        {
-            return "Animations/p1_player_0";
+            loaddir = "Animations/p4_PLAYER";
         }
+        else
+        {
+            loaddir = "Animations/p1_player_0";
+        }
+        m_animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(loaddir);
     }
 }
